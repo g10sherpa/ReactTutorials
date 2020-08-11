83b408f2e96a974768ca31912431eb1f32fe69b8
gyamzo sherpa <megyamzo@gmail.com>
2020-08-11 15:16:20 +0545

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import {add,subtract,multiply,divide} from './calculator';
+
+// const img = "https://picsum.photos/200";
+// const customStyle = {
+// color: "red",
+// fontSize: "20px",
+// border: "1px solid black"
+// };
+
+// customStyle.color = "blue";
+
+// ReactDOM.render(
+// <div>
+//   <h1 style={customStyle}>My Favourite Foods</h1>
+//   <img alt="random" src={img + "?grayscale" } />
+
+//   <img className="food-img" alt="bacon"
+//     src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg?crop=0.645xw:0.967xh;0.170xw,0.0204xh&resize=480:*" />
+//   <img className="food-img" alt="jamon"
+//     src="https://images-na.ssl-images-amazon.com/images/I/71lNrnbMXsL._SL1200_.jpg" />
+//   <img className="food-img" alt="noodles"
+//     src="https://www.errenskitchen.com/wp-content/uploads/2014/04/quick-and-easy-chinese-noodle-soup3-1.jpg" />
+// </div>,
+// document.getElementById("root")
+// );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ul>
+    <li>{add(1, 3)}</li>
+    <li>{multiply(1, 3)}</li>
+    <li>{subtract(8, 3)}</li>
+    <li>{divide(9, 3)}</li>
+  </ul>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
